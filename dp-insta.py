e74b2d20c89833289a6c79a8657b9a7e50fa963c
Cat love coffee <142666818+u0a316@users.noreply.github.com>
2025-07-04 10:46:32 +0700

    Update dp-insta.py

--- dp-insta.py
+++ dp-insta.py
@@ -2,7 +2,7 @@
 
 import instaloader
  
-ig = instaloader.Instaloader()
-dp = input("Enter Insta username : ")
+ig = instaloader.Instaloader() # aliasing module to ig
+dp = input("Enter Insta username : ") # create input()
  
-ig.download_profile(dp , profile_pic_only=True)
+ig.download_profile(dp , profile_pic_only=True) # proccess input to make it download work

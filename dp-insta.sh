bde57f2ef80dc87d476a6d10a12572e2edff4f24
Coffee Cat <142666818+u0a316@users.noreply.github.com>
2025-07-25 07:02:19 +0700

    Update dp-insta.sh

--- dp-insta.sh
+++ dp-insta.sh
@@ -1,5 +1,7 @@
+#/usr/bin/sh 
 function main() {
 eval 'cd $(dirname $0)'
 eval 'pip install -r requirements.txt'
 eval 'python3 dp-insta.py'
- }                                                               main
+}
+main
